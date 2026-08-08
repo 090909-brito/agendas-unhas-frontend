9844f9c6a38b1c72ad684b80fc60651ec5ff134a
090909-brito <ederbrito06@gmail.com>
2026-08-08 12:49:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1013,8 +1013,10 @@ export default function App() {
                   <img
                     src={LogoImg}
                     alt={SALON_NAME}
-                    style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "50% 30%", transformOrigin: "50% 30%" }}
+                    style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "50% 30%", transformOrigin: "50% 30%", transform: "scale(1.8)" }}
                     />
+                  <div>
+                  </div>
                 <div>
                   <div className="brand-name">{SALON_NAME}</div>
                   <div className="brand-sub">Carol &amp; Suelen · agende seu horário</div>
